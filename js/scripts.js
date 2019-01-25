f15e9f6e52794479da69685b5d0b3b07dfc9005a
Tien Le <its.just.tien@gmail.com>
2019-01-25 10:43:00 -0800

    add second loop for to set conditional

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,31 @@
 function test(numInput) {
 
   var numCounts = [];
+  var machineOut = [];
 
 
-  for (var i = 0; i < numInput; i ++) {
+  for (var i = -1; i < numInput; i ++) {
     numCounts.push(i + 1);
     console.log(numCounts);
   }
 
-// return numOutput;
+  for (var i = 0; i < numInput; i++) {
+    if (numCounts[i] == 1) {
+      machineOut[i] = "beep";
+    }
+    else if (numCounts[i] == 2) {
+      machineOut[i] = "boop";
+    }
+    else if (numCounts[i] == 3) {
+      machineOut[i] = "sorry";
+    }
+    else {
+      machineOut[i] = numCounts[i]
+    }
+    console.log(machineOut)
+  }
+
+// return numCounts
 
 
 };
